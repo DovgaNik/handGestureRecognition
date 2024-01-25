e293473294169ac41f2769d5fd16fa3dd7b1f691
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-01-25 15:55:30 +0200

    IT WORKS

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-#https://medium.com/@odil.tokhirov/how-i-built-a-hand-gesture-recognition-model-in-python-part-1-db378cf196e6
-import mediapipe as mp
 import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+model_path = 'data/gesture_recognizer.task'
 
-capture = cv2.VideoCapture(0)
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
 
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-  while capture.isOpened():
-      ret, frame = capture.read()
-      frame = cv2.flip(frame, 1)
-      image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-      detected_image = hands.process(image)
-      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-  
-      if detected_image.multi_hand_landmarks:
-          for hand_lms in detected_image.multi_hand_landmarks:
-              mp_drawing.draw_landmarks(image, hand_lms,
-                                        mp_hands.HAND_CONNECTIONS,
-                                        landmark_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
-                                            color=(255, 0, 255), thickness=4, circle_radius=2),
-                                        connection_drawing_spec=mp.solutions.drawing_utils.DrawingSpec(
-                                            color=(20, 180, 90), thickness=2, circle_radius=2)
-                                        )
-  
-      cv2.imshow('Webcam', image)
-  
-      if cv2.waitKey(1) & 0xFF == ord('q'):
+# Create a gesture recognizer instance with the video mode:
+options = GestureRecognizerOptions(
+    base_options = BaseOptions(model_path),
+    running_mode = VisionRunningMode.VIDEO,
+    num_hands = 2)
+with GestureRecognizer.create_from_options(options) as recognizer:
+    capture = cv2.VideoCapture(0)
+    while capture.isOpened():
+        ret, frame = capture.read()
+        frame = cv2.flip(frame, 1)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+        
+        gesture_recognition_result = recognizer.recognize_for_video(mp_image, int(capture.get(cv2.CAP_PROP_POS_MSEC)))
+        
+        try:
+            top_gesture = gesture_recognition_result.gestures[0][0]
+            print(top_gesture)
+        except:
+            pass
+
+
+
+
+        cv2.imshow('Webcam', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
           break
-
-capture.release()
-cv2.destroyAllWindows()
+    
+    capture.release()
+    cv2.destroyAllWindows()
